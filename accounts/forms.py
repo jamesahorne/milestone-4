1cc90721f33aa78c5051a2bd7eed739f872f0516
James Horne <james99h39@gmail.com>
2019-03-19 12:37:18 +0000

    Add class UserRegistrationForm with validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,37 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class UserLoginForm(forms.Form):
     ''' Login form '''
-
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(UserCreationForm):
+    ''' Register new user form '''
+    password1 = forms.CharField(label="Password",
+                                widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password",
+                                widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(u'This email address is already taken')
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if not password1 or not password2:
+            raise forms.ValidationError("Please confirm your password")
+        if password1 != password2:
+            raise ValidationError("Passwords must match")
+        return password2
