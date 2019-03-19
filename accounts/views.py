fb6594266cd7ab5c41b7c377f7b841748809a00c
James Horne <james99h39@gmail.com>
2019-03-19 10:44:49 +0000

    Add login_required to def logout and authentication to def login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 
@@ -8,7 +9,7 @@ def index(request):
 
     return render(request, 'index.html')
 
-
+@login_required
 def logout(request):
     ''' Logout '''
 
@@ -20,6 +21,9 @@ def logout(request):
 def login(request):
     ''' Login '''
 
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
         
@@ -30,6 +34,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in.")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect.")
     
