16840614c4ad88668d09bbbe56708f6012583012
James Horne <james99h39@gmail.com>
2019-03-21 12:52:05 +0000

    Add cart urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -6,12 +6,13 @@ from products import urls as products_urls
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
+from cart import urls as cart_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(products_urls)),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
-    
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
+    url(r'^cart/', include(cart_urls))
 ]
