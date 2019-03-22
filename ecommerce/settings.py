384e979adedf91f89e941d9c30e31e4aeb74c301
James Horne <james99h39@gmail.com>
2019-03-22 11:39:58 +0000

    Add search and checkout apps to INSTALLED_APPS, import env and add stripe api keys

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,7 +42,9 @@ INSTALLED_APPS = [
     'accounts',
     'home',
     'products',
-    'cart'
+    'cart',
+    'search',
+    'checkout'
 ]
 
 MIDDLEWARE = [
@@ -135,6 +138,9 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
