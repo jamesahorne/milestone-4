405f84a80a20d8f28ebc1216daaf0d49d76d9fb7
James Horne <james99h39@gmail.com>
2019-03-26 16:13:11 +0000

    Update AWS_S3_REGION_NAME to correct value

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,7 +145,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=94608000'
 }
 AWS_STORAGE_BUCKET_NAME = 'django-milestone'
-AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_REGION_NAME = 'eu-west-2'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
