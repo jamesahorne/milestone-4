d93082120d17d080cda109bd39a096053d47cbda
James Horne <james99h39@gmail.com>
2019-04-03 11:56:46 +0000

    Add About and Data views

--- home/views.py
+++ home/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 def index(request):
     ''' Index page '''
     return render(request, 'index.html')
+
+
+def about(request):
+    ''' About page '''
+    return render(request, 'about.html')
+
+
+def data(request):
+    ''' Data page '''
+    return render(request, 'data.html')
