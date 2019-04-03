5d575e02132e368c871dc01c4008dff004d9a92e
James Horne <james99h39@gmail.com>
2019-04-03 14:40:05 +0000

    Delete all_products and add all_tickets, buy_tickets, bug, feature and full_details

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_tickets, buy_tickets, bug, feature, full_detail
 
 urlpatterns = [
-    url(r'^$', all_products, name='products')
+    url(r'^$', all_tickets, name='all_tickets'),
+    url(r'^buy_tickets/$', buy_tickets, name='buy_tickets'),
+    url(r'^bug/$', bug, name='bug'),
+    url(r'^feature/$', feature, name='feature'),
+    url(r'^full_detail/$', full_detail, name='full_detail')
 ]
