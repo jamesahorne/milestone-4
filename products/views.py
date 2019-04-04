ed522c00c26dd265e8f982de7b39b3650f394e69
James Horne <james99h39@gmail.com>
2019-04-04 09:33:01 +0000

    Delete all_products view and add all_tickets, buy_tickets, bug, feature and full_details views

--- products/views.py
+++ products/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from .models import Product
 
-def all_products(request):
+
+def all_tickets(request):
     products = Product.objects.all()
-    return render(request, 'products.html', {"products": products})
+    return render(request, 'all_tickets.html', {"products": products})
+
+
+def buy_tickets(request):
+    products = Product.objects.all()
+    return render(request, 'buy_tickets.html', {"products": products})
+
+
+def bug(request):
+    products = Product.objects.all()
+    return render(request, 'bug.html', {"products": products})
+
+
+def feature(request):
+    products = Product.objects.all()
+    return render(request, 'feature.html', {"products": products})
+
+
+def full_detail(request):
+    products = Product.objects.all()
+    return render(request, 'full_detail.html', {"products": products})
