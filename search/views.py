058f11fcc4885661fa9b90a824bf4107ad0b3b5c
James Horne <james99h39@gmail.com>
2019-04-04 09:42:26 +0000

    Update def search render url to all_tickets

--- search/views.py
+++ search/views.py
@@ -4,4 +4,4 @@ from products.models import Product
 
 def search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, 'products.html', {"products": products})
+    return render(request, 'all_tickets.html', {"products": products})
