f39b3c14a36e9709fc42dfa19d7f6a2592af22d0
James Horne <james99h39@gmail.com>
2019-04-08 16:47:55 +0000

    Remove quantity from class OrderLineItem

--- checkout/models.py
+++ checkout/models.py
@@ -3,7 +3,6 @@ from products.models import Product
 
 
 class Order(models.Model):
-    
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
@@ -19,11 +18,8 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    
     order = models.ForeignKey(Order, null=False)
     product = models.ForeignKey(Product, null=False)
-    quantity = models.IntegerField(blank=False)
 
     def __str__(self):
-        return "{0} {1} @ {2}".format(self.quantity, self.product.name,
-                                      self.product.price)
+        return "{0} @ {1}".format(self.product.name, self.product.price)
