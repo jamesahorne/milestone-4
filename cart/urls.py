ba9bc7a0c1005dddfbd5a40a484d83d944ca7551
James Horne <james99h39@gmail.com>
2019-04-08 16:42:35 +0000

    Change edit_cart to delete_cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from .views import view_cart, add_to_cart, edit_cart
+from .views import view_cart, add_to_cart, delete_cart
 
 urlpatterns = [
     url(r'^$', view_cart, name='view_cart'),
     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
-    url(r'^edit/(?P<id>\d+)', edit_cart, name='edit_cart')
+    url(r'^delete/(?P<id>\d+)', delete_cart, name='delete_cart')
 ]
