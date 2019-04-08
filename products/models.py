4ea83cb4033361fc3c4874218afd7662ed1b6a49
James Horne <james99h39@gmail.com>
2019-04-08 17:29:59 +0000

    Merge BugTicket and FeatureTicket into just Ticket

--- products/models.py
+++ products/models.py
@@ -12,39 +12,21 @@ class Product(models.Model):
         return self.name
 
 
-class BugTicket(models.Model):
+class Ticket(models.Model):
 
-    STATUS_CHOICES = (('todo', 'ToDo'), ('doing', 'Doing'), ('done', 'Done'))
+    TYPE_CHOICES = (('Bug', 'Bug'), ('Feature', 'Feature'))
+    STATUS_CHOICES = (('Todo', 'ToDo'), ('Doing', 'Doing'), ('Done', 'Done'))
 
-    bug_issue_type = models.CharField(max_length=7, default='Bug',
-                                      editable=False)
-    bug_name = models.CharField(max_length=30, default='')
-    bug_description = models.TextField()
-    bug_status = models.CharField(max_length=5, choices=STATUS_CHOICES,
+    type = models.CharField(max_length=7, choices=TYPE_CHOICES,
+                                      default='Bug')
+    issue_name = models.CharField(max_length=30, default='')
+    description = models.TextField()
+    status = models.CharField(max_length=5, choices=STATUS_CHOICES,
                               default="ToDo")
-    bug_urgent = models.BooleanField()
-    bug_published_date = models.DateTimeField(blank=True, null=True,
+    urgent = models.BooleanField()
+    published_date = models.DateTimeField(blank=True, null=True,
                                           default=timezone.now)
-    bug_views = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.issue_name
-
-
-class FeatureTicket(models.Model):
-
-    STATUS_CHOICES = (('todo', 'ToDo'), ('doing', 'Doing'), ('done', 'Done'))
-
-    feature_type = models.CharField(max_length=7, default='Feature',
-                                      editable=False)
-    feature_name = models.CharField(max_length=30, default='')
-    feature_description = models.TextField()
-    feature_status = models.CharField(max_length=5, choices=STATUS_CHOICES,
-                              default="ToDo")
-    feature_urgent = models.BooleanField()
-    feature_published_date = models.DateTimeField(blank=True, null=True,
-                                          default=timezone.now)
-    feature_views = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.issue_name
