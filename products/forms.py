2492c1fd6c4a8fb2f9dcd5dc68e030721c2fbcd8
James Horne <james99h39@gmail.com>
2019-04-08 17:27:21 +0000

    Merge BugTicketForm and FeatureTicketForm into just TicketForm

--- products/forms.py
+++ products/forms.py
@@ -1,17 +1,8 @@
 from django import forms
-from .models import BugTicket, FeatureTicket
+from .models import Ticket
 
 
-class BugTicketForm(forms.ModelForm):
+class TicketForm(forms.ModelForm):
     class Meta:
-        model = BugTicket
-        fields = ('bug_issue_type', 'bug_issue_name', 'bug_issue_description',
-                  'bug_status', 'bug_urgent')
-
-
-class FeatureTicketForm(forms.ModelForm):
-    class Meta:
-        model = FeatureTicket
-        fields = ('feature_issue_type', 'feature_issue_name',
-                  'feature_issue_description', 'feature_status',
-                  'feature_urgent')
+        model = Ticket
+        fields = ('type', 'issue_name', 'description', 'status', 'urgent')
