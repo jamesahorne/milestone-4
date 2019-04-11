95694d8e2eb5d4f90e0c3e39a9ab9eecfae6e7ed
James Horne <james99h39@gmail.com>
2019-04-11 10:33:42 +0000

    Add login_required, in all_tickets change product to ticket, in bug and feature add form validation, in full_details add views count

--- products/views.py
+++ products/views.py
@@ -1,27 +1,51 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Product, Ticket
+from .forms import TicketForm
 
 
 def all_tickets(request):
-    products = Product.objects.all()
-    return render(request, 'all_tickets.html', {"products": products})
+    tickets = Ticket.objects.all()
+    return render(request, 'all_tickets.html', {"tickets": tickets})
 
 
+@login_required
 def buy_tickets(request):
     products = Product.objects.all()
     return render(request, 'buy_tickets.html', {"products": products})
 
 
+@login_required
 def bug(request):
-    products = Product.objects.all()
-    return render(request, 'bug.html', {"products": products})
+    if request.method == "POST":
+        ticket_form = TicketForm(request.POST)
+        if ticket_form.is_valid():
+            ticket_form.save()
+            messages.success(request, "Your ticket was saved successfully.")
+        else:
+            messages.error(request, "There was an error, your ticket was not saved successfully.")
+    else:
+        ticket_form = TicketForm
+    return render(request, 'bug.html', {"ticket_form": ticket_form})
 
 
+@login_required
 def feature(request):
-    products = Product.objects.all()
-    return render(request, 'feature.html', {"products": products})
+    if request.method == "POST":
+        ticket_form = TicketForm(request.POST)
+        if ticket_form.is_valid():
+            ticket_form.save()
+            messages.success(request, "Your ticket was saved successfully.")
+        else:
+            messages.error(request, "There was an error, your ticket was not saved successfully.")
+    else:
+        ticket_form = TicketForm
+    return render(request, 'feature.html', {"ticket_form": ticket_form})
 
 
-def full_detail(request):
-    products = Product.objects.all()
-    return render(request, 'full_detail.html', {"products": products})
+def full_detail(request, pk):
+    ticket = get_object_or_404(Ticket, pk=pk)
+    ticket.views +=1
+    ticket.save()
+    return render(request, 'full_detail.html', {"ticket": ticket})
