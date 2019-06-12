d9992e4f9b636da9c55e2901610c25484de65330
James Horne <james99h39@gmail.com>
2019-06-12 10:39:03 +0000

    Change search for products to search for tickets

--- search/views.py
+++ search/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Ticket
 
 
 def search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
-    return render(request, 'all_tickets.html', {"products": products})
+    tickets = Ticket.objects.filter(issue_name__icontains=request.GET['q'])
+    return render(request, 'all_tickets.html', {"tickets": tickets})
