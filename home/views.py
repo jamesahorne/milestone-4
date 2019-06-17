07cd3dc2168d59d1e7afebcc595c3e95483f4a39
James Horne <james99h39@gmail.com>
2019-06-17 09:54:13 +0000

    Delete unnecessary code

--- home/views.py
+++ home/views.py
@@ -1,12 +1,9 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 from products.models import Ticket
 from django.core import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-# Do I need this?
-from django.contrib.auth.models import User
 
 def index(request):
     ''' Index page '''
@@ -23,18 +20,10 @@ def data(request):
     return render(request, 'data.html')
 
 
-# def get_data(request):
-#     # tickets = Ticket.objects.all()
-#     tickets = serializers.serialize('json', Ticket.objects.all(), fields=('type', 'status', 'urgent'))
-
-#     return HttpResponse(tickets)
-
-
 class DataVisualisation(APIView):
     authentication_classes = []
     permission_classes = []
 
     def get(self, request, format=None):
         tickets = serializers.serialize('json', Ticket.objects.all(), fields=('type', 'status', 'urgent'))
-        # usernames = [user.username for user in User.objects.all()]
         return Response(tickets)
