a865ad486e54807cc2eb972e5d3eaee2eeacbc6b
James Horne <james99h39@gmail.com>
2019-06-17 10:05:17 +0000

    Add status and type charts and delete unnecessary code

--- static/js/data.js
+++ static/js/data.js
@@ -1,39 +1,72 @@
+"use strict";
+
 var endpoint = '/api/graphs/data/';
 
 $.ajax({
     method: 'GET',
     url: endpoint,
     success: function(data) {
-        console.log(`var data = ${data}`);
         var tickets = JSON.parse(data);
         charts(tickets);
     },
-    error: function(error_tickets) {
-        console.log(error);
-        console.log(error_tickets);
-    }
 });
 
-// Chart.js code
+var options = {scales: {yAxes: [{ticks: {beginAtZero: true}}]}};
+
+
+// Make bar colors all white? Then make var backgroundColor and var borderColor
+
+// Should each chart be in a separate function and not all in charts()?
+
+
 function charts(tickets) {
-    var urgent_tickets = [];
+    var ticket_statuses = [];
     for (var i = 0; i < tickets.length; i++) {
-        urgent_tickets.push(tickets[i].fields.urgent);
+        ticket_statuses.push(tickets[i].fields.status);
     }
-    var urgent_true = urgent_tickets.filter(Boolean).length;
-    var urgent_false = urgent_tickets.length - urgent_true;
-    
-    var ctx = $('#urgent');
+    var todo_count = ticket_statuses.filter(status => status === 'Todo').length;
+    var doing_count = ticket_statuses.filter(status => status === 'Doing').length;
+    var done_count = ticket_statuses.filter(status => status === 'Done').length;
+    var ctx = $('#status');
     var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['Todo', 'Doing', 'Done'],
+            datasets: [{
+                label: 'Type',
+                data: [todo_count, doing_count, done_count],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            grid: false
+        }
+    });
+
+    var ticket_types = [];
+    for (var i = 0; i < tickets.length; i++) {
+        ticket_types.push(tickets[i].fields.type);
+    }
+    var bug_count = ticket_types.filter(type => type === 'Bug').length;
+    var feature_count = ticket_types.length - bug_count;
+    var ctx1 = $('#type');
+    var myChart = new Chart(ctx1, {
         type: 'bar',
         data: {
-            labels: ['Urgent', 'Not Urgent'], // tickets_urgent,
+            labels: ['Bug', 'Feature'],
             datasets: [{
-                label: 'Urgent',
-                
-                // Need to get tickets_urgent to be a number
-                
-                data: [urgent_true, urgent_false],
+                label: 'Type',
+                data: [bug_count, feature_count],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
@@ -45,54 +78,34 @@ function charts(tickets) {
                 borderWidth: 1
             }]
         },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+        options: options
+    });
+
+    var ticket_urgencies = [];
+    for (var i = 0; i < tickets.length; i++) {
+        ticket_urgencies.push(tickets[i].fields.urgent);
+    }
+    var is_urgent_count = ticket_urgencies.filter(Boolean).length;
+    var not_urgent_count = ticket_urgencies.length - is_urgent_count;
+    var ctx2 = $('#urgent');
+    var myChart = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: ['Urgent', 'Not Urgent'],
+            datasets: [{
+                label: 'Urgency',
+                data: [is_urgent_count, not_urgent_count],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: options
     });
 }
-    
-
-// Chart.js documentation example
-// var ctx2 = document.getElementById('type').getContext('2d');
-// var myChart2 = new Chart(ctx2, {
-//     type: 'bar',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [7, 9, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
