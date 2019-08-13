66343c1cb24e44b947ce7ba754e272908d7d379c
James Horne <james99h39@gmail.com>
2019-08-13 10:56:32 +0000

    Add edit ticket url

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
-from .views import all_tickets, buy_tickets, bug, feature, full_detail, feature_or_upvote, upvote, add_upvote
+from .views import all_tickets, buy_tickets, bug, edit_ticket, feature, full_detail, feature_or_upvote, upvote, add_upvote
 
 urlpatterns = [
     url(r'^$', all_tickets, name='all_tickets'),
     url(r'^buy-tickets/$', buy_tickets, name='buy_tickets'),
     url(r'^bug/$', bug, name='bug'),
+    url(r'^edit-ticket/(?P<pk>\d+)$', edit_ticket, name='edit_ticket'),
     url(r'^feature/$', feature, name='feature'),
     url(r'^full-detail/(?P<pk>\d+)$', full_detail, name='full_detail'),
     url(r'^feature-or-upvote/$', feature_or_upvote, name='feature_or_upvote'),
