b36f973b8ccedb187081e8afbfb855182dcbb9a1
James Horne <james99h39@gmail.com>
2019-08-15 14:13:03 +0000

    Remove unnecessary get_data

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
-from .views import index, about, data, DataVisualisation # ,get_data
+from .views import index, about, data, DataVisualisation
 
 
 urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^about/$', about, name="about"),
     url(r'^data/$', data, name="data"),
-#    url(r'^api/data/$', get_data, name="get_data"),
     url(r'^api/graphs/data/$', DataVisualisation.as_view())
 ]
