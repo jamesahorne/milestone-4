823d0d441c8c6d898cd187a81ee3fbc17144ba4e
James Horne <james99h39@gmail.com>
2019-08-15 14:11:06 +0000

    Remove tests.py files

--- products/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from .models import Product
-
-class ProductTests(TestCase):
-    ''' Product model tests '''
-
-    def test_str(self):
-        test_name = Product(name='Product name')
-        self.assertEqual(str(test_name), 'Product name')
